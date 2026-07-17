5798580f3261dce90ffc1ebf220e0c4a105c1c41
yukinchu <yukinchu.kazu@gmail.com>
2026-07-17 16:10:04 +0900

    Refactor app initialization

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 /*
 =========================================
  Mountain GPV
- Version 0.2
+ Version 0.5.0
 =========================================
 */
 
 const app = {
-    version: "0.2.0",
+    version: "0.5.0",
     mountains: []
 };
 
+// ----------------------------
+// アプリ起動
+// ----------------------------
+
 window.onload = async () => {
 
-    console.log("Mountain GPV Started");
+    console.log("=================================");
+    console.log(" Mountain GPV");
+    console.log(" Version " + app.version);
+    console.log("=================================");
 
     await loadMountains();
 
     initializeSearch();
+
+    console.log("Application Ready");
 
 };
 
@@ -30,73 +39,25 @@ async function loadMountains() {
 
         const response = await fetch("mountains.json");
 
+        if (!response.ok) {
+            throw new Error("mountains.json を読み込めません");
+        }
+
         app.mountains = await response.json();
 
-        console.log(app.mountains.length + " mountains loaded.");
+        console.log(
+            app.mountains.length +
+            " mountains loaded."
+        );
 
-    } catch (error) {
+    }
+
+    catch (error) {
 
         console.error(error);
 
-    }
-
-}
-
-// ----------------------------
-// 検索
-// ----------------------------
-
-function initializeSearch() {
-
-    const input = document.getElementById("mountain-search");
-
-    input.addEventListener("input", searchMountain);
-
-}
-
-// ----------------------------
-// 検索実行
-// ----------------------------
-
-function searchMountain(event) {
-
-    const keyword = event.target.value.toLowerCase();
-
-    if (keyword === "") {
-
-        document.getElementById("mountain-name").textContent =
-            "山を選択してください";
-
-        document.getElementById("mountain-height").textContent =
-            "標高 --";
-
-        return;
+        alert("山データの読込に失敗しました。");
 
     }
-
-    const result = app.mountains.find(mountain =>
-
-        mountain.name.includes(keyword) ||
-        mountain.reading.includes(keyword)
-
-    );
-
-    if (!result) {
-
-        document.getElementById("mountain-name").textContent =
-            "見つかりません";
-
-        document.getElementById("mountain-height").textContent =
-            "";
-
-        return;
-
-    }
-
-    document.getElementById("mountain-name").textContent =
-        result.name;
-
-    document.getElementById("mountain-height").textContent =
-        `標高 ${result.elevation} m`;
 
 }
