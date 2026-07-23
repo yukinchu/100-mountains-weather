8626711e98e6e713dcc7d8303372b22b9d19844b
yukinchu <yukinchu.kazu@gmail.com>
2026-07-23 10:56:39 +0900

    Create dedicated GPV module

---
+++ gpv.js
@@ -0,0 +1,20 @@
+/*
+=========================================
+ Mountain GPV
+ GPV Module
+ Version 1.0.0
+=========================================
+*/
+
+// ----------------------------
+// GPV取得
+// （現在はダミー）
+// ----------------------------
+
+async function loadGPV(latitude, longitude) {
+
+    console.log("GPV MODE");
+
+    throw new Error("GPV API 未実装");
+
+}
