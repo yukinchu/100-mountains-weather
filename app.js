8f41cf40033e380e0133e8e92b1403824b62e0d8
yukinchu <yukinchu.kazu@gmail.com>
2026-09-01 16:20:10 +0900

    Remove date from labels in chart drawing

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ async function showWeather() {
 
   buildStrip(h);
 
-  const labels = h.time.map(t => fmtDate(t) + " " + fmtHour(t) + "時");
+  const labels = h.time.map(t => fmtHour(t) + "時");
   drawChart1(labels, h.cloudcover, h.precipitation);
   drawCloud("chartLow", labels, h.cloudcover_low, "下層雲量（%）", "#e91e8c");
   drawCloud("chartMid", labels, h.cloudcover_mid, "中層雲量（%）", "#2196f3");
